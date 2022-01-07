cbaa9abcb36107355f4b9eb128af6ddcaf6ea15b
mariosan <sanmartin.mario@outlook.com>
2022-01-07 20:53:02 -0300

    ejercicio 31 mas completo

--- ejercicio31.php
+++ ejercicio31.php
@@ -1,13 +1,24 @@
 <?php
 
 $txtNombre="";
-
 $radioLenguaje="";
+$chkphp="";
+$chkhtml="";
+$chkjs="";
+
+$listaPersonajes="";
+$aprendiendo="";
 if ($_POST){
     $txtNombre= (isset($_POST['txtNombre']))?$_POST['txtNombre']:"";
     $radioLenguaje= (isset($_POST['lenguaje']))?$_POST['lenguaje']:"";
+    
+    $chkphp = ( isset($_POST['chkphp'])=="si")?"checked":"";
+    $chkhtml = ( isset($_POST['chkhtml'])=="si")?"checked":"";
+    $chkjs = ( isset($_POST['chkjs'])=="si")?"checked":"";
 
-    print_r($radioLenguaje);
+    $listaPersonajes= (isset($_POST['listaPersonajes']))?$_POST['listaPersonajes']:"";
+    $aprendiendo = (isset($_POST['aprendiendo']))?$_POST['aprendiendo']:"";
+    // print_r($_POST);
 }
 
 ?>
@@ -23,10 +34,19 @@ if ($_POST){
 <body>
     
     <strong> Hola:  </strong> <?php  echo $txtNombre;?> <br/>
-    <strong> El lenguaje seleccionado es:  </strong> <?php  echo $radioLenguaje;?> 
-
+    <strong> (Radio)El lenguaje seleccionado es:  </strong> <?php  echo $radioLenguaje;?><br>
+    <strong> (Checkbox)Los lenguajes a estudiar son :  </strong> 
+        <br><?php  echo ($chkphp=="checked"?"PHP":"");?> 
+        <br><?php  echo ($chkhtml=="checked"?"HTML":"");?> 
+        <br><?php  echo ($chkjs=="checked"?"JS":"");?> 
+    <br>
+    <br>    
+    <i>Tu Personaje de OnePunchMan es: </i> <?php echo $listaPersonajes; ?>
+    <br>
+    <hr>
+    <i>Estas aprendiendo : </i> <?php echo $aprendiendo;?>
     <form action="ejercicio31.php" method="post">
-        
+        <br>Ingrese un texto: 
         <input type="text" name="txtNombre" value="<?php echo $txtNombre; ?>">
         <br/>
         ¿Cual te gusta mas?: <br/>
@@ -35,7 +55,27 @@ if ($_POST){
         <input type="radio" <?php echo ($radioLenguaje=="html"?"checked":""); ?> name="lenguaje" value="html" id=""> HTML
         <br/>
         <input type="radio" <?php echo ($radioLenguaje=="js"?"checked":""); ?> name="lenguaje" value="js" id=""> JS
-        <br/>   
+        <br/>
+        ¿Cuales estudiaras?<br/>
+        PHP <input type="checkbox" <?php echo $chkphp?> name="chkphp" value="si" id=""><br/>
+        HTML<input type="checkbox" <?php echo $chkhtml?> name="chkhtml" value="si" id=""><br/>
+        JS  <input type="checkbox" <?php echo $chkjs?> name="chkjs" value="si" id=""><br/>
+        
+        <hr>
+        <strong>Personaje de One PunchMan</strong>
+        <select name="listaPersonajes" id="">
+            <option value=""></option>
+            <option value="Saitama" <?php echo ($listaPersonajes=="Saitama"?"selected":""); ?> >Saitaman</option>
+            <option value="Genos" <?php echo ($listaPersonajes=="Genos"?"selected":""); ?> >Genos</option>
+            <option value="Garou" <?php echo ($listaPersonajes=="Garou"?"selected":""); ?> >Garou el kaijin</option>
+        </select>
+        <br>
+        <i>¿Que estas aprendiendo?</i>
+        <br>
+        <textarea name="aprendiendo" id="" cols="30" rows="10">
+            <?php echo $aprendiendo;?>
+        </textarea>
+        <br>
         <input type="submit" value="Enviar">
     </form>
 </body>
